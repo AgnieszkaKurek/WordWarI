08c4eb7b6ebf85eb97fc4fa9a3ccdc95443b6191
Agnieszka Kurek <agniechakurek@gmail.com>
2016-12-19 17:37:00 +0100

    adding empty lines separate  functions #25

--- scripts/slider.js
+++ scripts/slider.js
@@ -3,6 +3,7 @@
 var number = Math.floor(Math.random() * 22) + 1;
 var timer1 = 0;
 var timer2 = 0;
+
 function setSlide(noSlide) {
     clearTimeout(timer1);
     clearTimeout(timer2);
@@ -10,9 +11,11 @@ function setSlide(noSlide) {
     hide();
     setTimeout("changeSlide()", 500);
 }
+
 function hide() {
     $("#slider").fadeOut(500);
 }
+
 function changeSlide() {
     number++; if (number > 22) number = 1;
     var file =`<img src="../images/slides/slide${number}.jpg"/>` ;
