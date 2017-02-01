171da47897705bda11e41cb405066124c2c29279
Agnieszka Kurek <agniechakurek@gmail.com>
2017-02-01 08:55:26 +0100

    formating document

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,16 +1,17 @@
 'use strict';
+
 var numberOfSlides = 22; 
 var number = Math.floor(Math.random() * numberOfSlides) + 1;
 var timer1 = 0;
 var timer2 = 0;
 
- function setSlide(noSlide) {
-      clearTimeout(timer1);
-      clearTimeout(timer2);
-      number = noSlide - 1;
-      hide();
-      setTimeout("changeSlide()", 500);
- }
+function setSlide(noSlide) {
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    number = noSlide - 1;
+    hide();
+    setTimeout("changeSlide()", 500);
+}
 
 function hide() {
     $("#slider").fadeOut(500);
